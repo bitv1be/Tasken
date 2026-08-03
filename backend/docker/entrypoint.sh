74b448b8fdf8056cd07ee2d050b756625992779c
SS1GGzxc <sterletivakiril@gmail.com>
2026-08-03 22:34:22 +0400

    feat: adding building images and publish to github

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -2,22 +2,20 @@
 
 set -eu
 
-cd /var/www/html
+# cd /var/www/html
 
-mkdir -p \
-    storage/app/public \
-    storage/framework/cache/data \
-    storage/framework/sessions \
-    storage/framework/views \
-    storage/logs \
-    bootstrap/cache
+# mkdir -p \
+#     storage/app/public \
+#     storage/framework/cache/data \
+#     storage/framework/sessions \
+#     storage/framework/views \
+#     storage/logs \
+#     bootstrap/cache
 
 if [ "${LARAVEL_OPTIMIZE:-true}" = "true" ]; then
     echo "Optimizing Laravel..."
 
     php artisan optimize
 fi
-
-php artisan migrate
 
 exec "$@"
